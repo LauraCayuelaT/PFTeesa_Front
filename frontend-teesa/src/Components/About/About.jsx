46b9f3551e60bb51566064a3b72248bd77a06b2f
MariaSol <mariasolcomba@hotmail.com>
2023-06-01 17:58:32 -0300

    Componente About Completo

--- frontend-teesa/src/Components/About/About.jsx
+++ frontend-teesa/src/Components/About/About.jsx
@@ -1,9 +1,60 @@
-function About() {
+import React from 'react';
+import AboutDevCard from '../AboutDevCard/AboutDevCard';
+import FaLinkedin from "../../assets/LinkedIn_icon.svg.png"
+import FaGithub from "../../assets/images.png"
+import developers from "../../developersData";
+
+const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className="bg-white text-black">
+      <h1 className="text-3xl font-bold text-center mt-8">Acerca de los desarrolladores Web</h1>
+      <p className="text-center mt-4">
+        7 web developers que se unieron para desarrollar este proyecto de Colombia, uniendo los conocimientos de Base de Datos, Back-End y 
+        Front-End agregándole herramientas de diseño UX/UI para una mejor navegación de manera intuitiva.
+      </p>
+      <hr className="border-black-600" />
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+        <div className="col-span-2 md:col-span-4">
+          <div className="flex justify-center">
+            {developers.slice(0, 4).map((developer) => (
+              <AboutDevCard
+                key={developer.name}
+                name={developer.name}
+                country={developer.country}
+                linkedin={developer.linkedin}
+                github={developer.github}
+                image={developer.img ? developer.img : developer.image}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="col-span-2 md:col-span-4 flex justify-center">
+          {developers.slice(4, 7).map((developer) => (
+            <AboutDevCard
+              key={developer.name}
+              name={developer.name}
+              country={developer.country}
+              linkedin={developer.linkedin}
+              github={developer.github}
+              image={developer.img ? developer.img : developer.image}
+            />
+          ))}
+        </div>
+      </div>
+      <p className="text-center mt-4 text-black-800">
+        Haznos saber si deseas obtener más información sobre nuestro personal y su formación profesional.
+      </p>
+      <footer className="flex items-center justify-center bg-gray-200 py-4">
+        <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer" className="text-blue-700 mx-2">
+          <img src={FaLinkedin} alt="LinkedIn" className="w-6 h-6" />
+        </a>
+        <a href="https://github.com/" target="_blank" rel="noopener noreferrer" className="text-blue-700 mx-2">
+          <img src={FaGithub} alt="GitHub" className="w-6 h-6" />
+        </a>
+      </footer>
     </div>
   );
-}
+};
 
 export default About;
+
---
+++ frontend-teesa/src/developersData.js
@@ -0,0 +1,71 @@
+import SolDevImg from "./assets/SolDevCard.jpg";
+import JhonnyDevImg from "./assets/JhonnyDevCard.png";
+import LauraDevImg from "./assets/LauraDevCard.jpg";
+import ThiagoDevImg from "./assets/ThiagoDevCard.jpeg";
+import JuanDevImg from "./assets/JuanDevCard.jpg";
+import AndresDevImg from "./assets/AndresDevCard.jpg";
+import YoseliDevImg from "./assets/YoseliDevCard.jpg"
+
+const developers = [
+    {
+      name: "Maria Sol Comba Maiztegui",
+      country: "Argentina",
+      role: "Full Stack Web Developer",
+      linkedin: "https://www.linkedin.com/in/maria-sol-comba-maiztegui/",
+      github: "https://github.com/mariasolcba",
+      image: SolDevImg,
+    },
+    {
+      name: "Laura Cayuela",
+      country: "Colombia",
+      role: "Full Stack Web Developer",
+      linkedin: "https://www.linkedin.com/in/lauracayuelatovar/",
+      github: "https://github.com/lauracayuela",
+      image: LauraDevImg,
+    },
+    {
+        name: "Jhonny Zambrano Saavedra",
+        country: "Colombia",
+        role: "Full Stack Web Developer",
+        linkedin: "https://www.linkedin.com/in/jhonnyzamsa/",
+        github: "https://github.com/jhonnyzamsa",
+        image: JhonnyDevImg,
+      },
+      {
+        name: "Juan David Gustin Andino",
+        country: "Colombia",
+        role: "Full Stack Web Developer",
+        linkedin: "https://www.linkedin.com/in/juan-david-gustin-andino/",
+        github: "https://github.com/juangustinandino",
+        image: JuanDevImg,
+      },
+      {
+        name: "Tiago Bergmann",
+        country: "Argentina",
+        role: "Full Stack Web Developer",
+        linkedin: "https://www.linkedin.com/in/tiago-bergmann-0576b7264/",
+        github: "https://github.com/tiago-bergmann",
+        image: ThiagoDevImg,
+      },
+      {
+        name: "Yoseli Aguilera",
+        country: "Argentina",
+        role: "Full Stack Web Developer",
+        linkedin: "https://www.linkedin.com/in/yoseli-aguilera-2766a1218/",
+        github: "https://github.com/yoseliaguilera",
+        image: YoseliDevImg,
+      },
+      {
+        name: "Andrés Jose Paternina Rubiano",
+        country: "Colombia",
+        role: "Full Stack Web Developer",
+        linkedin: "https://www.linkedin.com/in/andr%C3%A9s-jose-paternina-rubiano-557309229/",
+        github: "https://github.com/andrespaternina",
+        image: AndresDevImg,
+      },
+
+  ];
+  
+
+  export default developers;
+  
