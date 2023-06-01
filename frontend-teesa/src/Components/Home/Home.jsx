e3b8de9a5c5df821e9deb68fa24b06307a7e234a
Juan Gustin <juandgustin@gmail.com>
2023-06-01 17:32:17 -0500

    Teesa Home - V1

--- frontend-teesa/src/Components/Home/Home.jsx
+++ frontend-teesa/src/Components/Home/Home.jsx
@@ -1,7 +1,15 @@
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='flex w-full h-full'>
+      {/* Second Navbar */}
+      <div className='flex bg-red-50 w-full m-0 items-center'>
+        <h2>Electrico</h2>
+        <h2>Gas</h2>
+        <h2>Refrigeración</h2>
+        <h2>Hornos</h2>
+        <h2>Respuestos</h2>
+        <h2>Panaderia</h2>
+      </div>
     </div>
   );
 }
--- frontend-teesa/tailwind.config.js
+++ frontend-teesa/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        landingb: '#cce3f5',
+        bluetext: '#3598dc',
+      },
+    },
   },
   plugins: [],
 };
